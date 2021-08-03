1e860f5ec3bd482277af29a0abb655d288370dd2
Leandro <ltoniut@gmail.com>
2021-08-02 22:38:29 -0300

    Empty file for git

--- src/typeorm-model-generator/generate-index.ts
+++ src/typeorm-model-generator/generate-index.ts
@@ -5,6 +5,7 @@ const generateIndex = async (): Promise<void> => {
   fs.readdir(path.join(__dirname, '/../typeorm/entities'), (err, files) => {
     fs.unlinkSync(path.join(__dirname, '/../schemas/model-builders/index.ts'));
     fs.unlinkSync(path.join(__dirname, '/../models/index.ts'));
+    fs.unlinkSync(path.join(__dirname, '/../models/.empty'));
 
     const modulesFile = fs.createWriteStream(path.join(__dirname, '/../../.module-variables'));
     const indexBuilderFile = fs.createWriteStream(path.join(__dirname, '/../schemas/model-builders/index.ts'));
